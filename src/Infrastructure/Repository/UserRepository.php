59bd3b1b3eefe453b8562ac7d88515578901b935
Alexandre Gérault <me@alexandre-gerault.fr>
2020-08-23 17:08:45 +0200

    feat(registration): Plug Doctrine
    Create DoctrineUser entity
    Implement Doctrine repository service into UserRepository and implements register method.
    
    Signed-off-by: Alexandre Gérault <me@alexandre-gerault.fr>

--- src/Infrastructure/Repository/UserRepository.php
+++ src/Infrastructure/Repository/UserRepository.php
@@ -2,13 +2,32 @@
 
 namespace App\Infrastructure\Repository;
 
+use App\Infrastructure\Entity\DoctrineUser;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use TFounder\Domain\Security\Entity\User;
 use TFounder\Domain\Security\Gateway\UserGateway;
 
-class UserRepository implements UserGateway
+class UserRepository extends ServiceEntityRepository implements UserGateway
 {
+    /**
+     * UserRepository constructor.
+     * @param ManagerRegistry $registry
+     */
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, DoctrineUser::class);
+    }
+
     public function register(User $user): void
     {
+        $doctrineUser = (new DoctrineUser())
+            ->setPseudonym($user->getPseudonym())
+            ->setEmail($user->getEmail())
+            ->setPassword($user->getPassword());
+
+        $this->_em->persist($doctrineUser);
+        $this->_em->flush();
     }
 
     public function isPseudonymAlreadyInUse($pseudonym): bool
